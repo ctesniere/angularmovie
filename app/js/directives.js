6b105332cc46cba0b6e3e49228fdc6491007678f
Cedric TESNIERE <tesniere.cedric@gmail.com>
2015-08-27 17:52:44 +0200

    soluce

--- app/js/directives.js
+++ app/js/directives.js
@@ -1,1 +1,16 @@
 "use strict";
+
+angularMovieApp.directive("movielt", function () {
+    return {
+        templateUrl: "partials/listMovies.html",
+        replace: true,
+        restrict: 'E',
+        scope: {
+            movie: '=mymovie'
+        },
+        transclude: true,
+        controller: function ($scope, $element, $attrs, $transclude) {
+            console.log($scope, $element, $attrs, $transclude);
+        }
+    };
+});
